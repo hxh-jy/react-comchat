82f73d159523e5472017f63b2a2f42727195b1b0
hanxuehong <hanxuehong@excean.com>
2022-09-23 14:48:16 +0800

    散点图的使用

--- src/pages/LearnEcharts/index.jsx
+++ src/pages/LearnEcharts/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 // import InitEcharts from '../../components/LearnEcharts/InitBar'
 // import InitLine from '../../components/LearnEcharts/initLine'
-import InitPie from '../../components/LearnEcharts/InitPie'
-import CustomStyle from '../../components/LearnEcharts/InitPie/CustomStyle'
+// import InitPie from '../../components/LearnEcharts/InitPie'
+// import CustomStyle from '../../components/LearnEcharts/InitPie/CustomStyle'
+
+import InitScatter from '../../components/LearnEcharts/InitScatter'
 export default function learnEcharts() {
     return (
         <>
@@ -13,10 +15,13 @@ export default function learnEcharts() {
             {/* <InitEcharts /> */}
 
             {/* 饼状图的基本使用 */}
-            <InitPie />
+            {/* <InitPie /> */}
 
             {/* 自定义样式的饼图 */}
-            <CustomStyle/>
+            {/* <CustomStyle/> */}
+
+            {/* 散点图的基本使用 */}
+            <InitScatter />
         </>
     )
 }
